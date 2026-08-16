bc8984495a4a1c82f8456962354988b344f25141
Viknesh <76156730+viknesh-ai@users.noreply.github.com>
2026-08-16 22:50:58 +0530

    Migrate tests/test_aampdist.py from npt.assert_almost_equal to npt.assert_allclose (#1184)

--- tests/test_aampdist.py
+++ tests/test_aampdist.py
@@ -44,9 +44,9 @@ def test_aampdist_vect(T_A, T_B):
     m = 3
     for p in [1.0, 2.0, 3.0]:
         ref_aampdist_vect = naive.aampdist_vect(T_A, T_B, m, p=p)
-        comp_aampdist_vect = _aampdist_vect(T_A, T_B, m, p=p)
+        cmp_aampdist_vect = _aampdist_vect(T_A, T_B, m, p=p)
 
-        npt.assert_almost_equal(ref_aampdist_vect, comp_aampdist_vect)
+        npt.assert_allclose(cmp_aampdist_vect, ref_aampdist_vect, atol=1.5e-07)
 
 
 @pytest.mark.parametrize("T_A, T_B", test_data)
@@ -54,9 +54,9 @@ def test_aampdist_vect(T_A, T_B):
 def test_aampdist_vect_percentage(T_A, T_B, percentage):
     m = 3
     ref_aampdist_vect = naive.aampdist_vect(T_A, T_B, m, percentage=percentage)
-    comp_aampdist_vect = _aampdist_vect(T_A, T_B, m, percentage=percentage)
+    cmp_aampdist_vect = _aampdist_vect(T_A, T_B, m, percentage=percentage)
 
-    npt.assert_almost_equal(ref_aampdist_vect, comp_aampdist_vect)
+    npt.assert_allclose(cmp_aampdist_vect, ref_aampdist_vect, atol=1.5e-07)
 
 
 @pytest.mark.parametrize("T_A, T_B", test_data)
@@ -64,9 +64,9 @@ def test_aampdist_vect_percentage(T_A, T_B, percentage):
 def test_aampdist_vect_k(T_A, T_B, k):
     m = 3
     ref_aampdist_vect = naive.aampdist_vect(T_A, T_B, m, k=k)
-    comp_aampdist_vect = _aampdist_vect(T_A, T_B, m, k=k)
+    cmp_aampdist_vect = _aampdist_vect(T_A, T_B, m, k=k)
 
-    npt.assert_almost_equal(ref_aampdist_vect, comp_aampdist_vect)
+    npt.assert_allclose(cmp_aampdist_vect, ref_aampdist_vect, atol=1.5e-07)
 
 
 @pytest.mark.parametrize("T_A, T_B", test_data)
@@ -74,9 +74,9 @@ def test_aampdist(T_A, T_B):
     m = 3
     for p in [1.0, 2.0, 3.0]:
         ref_mpdist = naive.aampdist(T_A, T_B, m, p=p)
-        comp_mpdist = aampdist(T_A, T_B, m, p=p)
+        cmp_mpdist = aampdist(T_A, T_B, m, p=p)
 
-        npt.assert_almost_equal(ref_mpdist, comp_mpdist)
+        npt.assert_allclose(cmp_mpdist, ref_mpdist, atol=1.5e-07)
 
 
 @pytest.mark.parametrize("T_A, T_B", test_data)
@@ -84,9 +84,9 @@ def test_aampdist(T_A, T_B):
 def test_aampdist_percentage(T_A, T_B, percentage):
     m = 3
     ref_mpdist = naive.aampdist(T_A, T_B, m, percentage=percentage)
-    comp_mpdist = aampdist(T_A, T_B, m, percentage=percentage)
+    cmp_mpdist = aampdist(T_A, T_B, m, percentage=percentage)
 
-    npt.assert_almost_equal(ref_mpdist, comp_mpdist)
+    npt.assert_allclose(cmp_mpdist, ref_mpdist, atol=1.5e-07)
 
 
 @pytest.mark.parametrize("T_A, T_B", test_data)
@@ -94,9 +94,9 @@ def test_aampdist_percentage(T_A, T_B, percentage):
 def test_aampdist_k(T_A, T_B, k):
     m = 3
     ref_mpdist = naive.aampdist(T_A, T_B, m, k=k)
-    comp_mpdist = aampdist(T_A, T_B, m, k=k)
+    cmp_mpdist = aampdist(T_A, T_B, m, k=k)
 
-    npt.assert_almost_equal(ref_mpdist, comp_mpdist)
+    npt.assert_allclose(cmp_mpdist, ref_mpdist, atol=1.5e-07)
 
 
 @pytest.mark.filterwarnings("ignore:numpy.dtype size changed")
@@ -109,6 +109,6 @@ def test_aampdisted(T_A, T_B, dask_cluster):
         m = 3
         for p in [1.0, 2.0, 3.0]:
             ref_mpdist = naive.aampdist(T_A, T_B, m, p=p)
-            comp_mpdist = aampdisted(dask_client, T_A, T_B, m, p=p)
+            cmp_mpdist = aampdisted(dask_client, T_A, T_B, m, p=p)
 
-            npt.assert_almost_equal(ref_mpdist, comp_mpdist)
+            npt.assert_allclose(cmp_mpdist, ref_mpdist, atol=1.5e-07)
